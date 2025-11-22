d6da03ac9ddb1700f239efc0c4351a987722e409
Igor Bazanov <itenthusiast.ru@gmail.com>
2025-11-22 14:04:14 +0700

    Add manage file

---
+++ geteway-manage.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+GATEWAY_DIR="/opt/api-gateway"
+
+case "$1" in
+    start)
+        cd $GATEWAY_DIR && docker-compose up -d
+        ;;
+    stop)
+        cd $GATEWAY_DIR && docker-compose down
+        ;;
+    restart)
+        cd $GATEWAY_DIR && docker-compose restart
+        ;;
+    status)
+        cd $GATEWAY_DIR && docker-compose ps
+        ;;
+    logs)
+        cd $GATEWAY_DIR && docker-compose logs -f
+        ;;
+    update)
+        cd $GATEWAY_DIR && docker-compose pull && docker-compose up -d
+        ;;
+    info)
+        echo "Gateway directory: $GATEWAY_DIR"
+        echo "Available commands: start, stop, restart, status, logs, update"
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|restart|status|logs|update|info}"
+        exit 1
+        ;;
+esac
